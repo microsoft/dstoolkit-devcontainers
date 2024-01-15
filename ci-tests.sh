f7154187cf2a09440a01342dd9975f0d203da783
Kosuke Fujimoto <fujikosu@gmail.com>
2024-01-15 17:29:46 +0900

    refactor: Consolidate tool related requirements files to requirements-dev.txt at root (#20)
    Currently python tool related package installation scatters across the codebase. This PR consolidates all of them into one requirements-dev.txt at root. This also applies changes to devcontainer.json, Dockerfile across the repo and CI pipeline yaml to reflect the update accordingly. Now all container's build context points to the root of directory to have access to requirements-dev.txt. So .dockerginore was moved to the root as well

--- ci-tests.sh
+++ ci-tests.sh
@@ -19,7 +19,7 @@ for test_dir_parent in $(find "${repo_root}/src" -type d -name 'tests' -exec dir
     count_test_py_files=$(find "${repo_root}/src/${test_dir_parent}/tests"/*.py 2>/dev/null | wc -l)
     if [ $count_test_py_files != 0 ]; then
         # Use the devcontainer Dockerfile to build a Docker image for the module to run tests
-        docker build "${repo_root}/src" -f "${repo_root}/src/${test_dir_parent}/.devcontainer/Dockerfile" -t "${test_dir_parent}"
+        docker build "${repo_root}" -f "${repo_root}/src/${test_dir_parent}/.devcontainer/Dockerfile" -t "${test_dir_parent}"
         
         echo "Running tests for ${test_dir_parent}, found ${count_test_py_files} test files"
         
